5b8832a36cf4b446d35892a623cd256133a1ad95
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-03-08 22:12:03 +0400

    Init app

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>Some more text</Text>
+    </View>
+  );
+};
+
+export default Home;
